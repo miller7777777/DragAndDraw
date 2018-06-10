ba4965f2ec701d27d16100c09dc696e525a8acd1
miller777 <miller777@mail.ru>
2018-06-10 15:07:54 +0300

    First commit;

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+class DragAndDrawFragment extends Fragment{
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+}
