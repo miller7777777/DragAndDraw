bae8ad9763faedaf020a23abc1c831f674510a37
miller777 <miller777@mail.ru>
2018-06-11 03:43:05 +0300

    Обработка касаний;

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,49 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
+
+    // Используется при создании представления в коде
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Используется при заполнении представления по разметке XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+        return true;
+    }
+}
